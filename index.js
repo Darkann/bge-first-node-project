3c189ee4b243cc78e26be1e519fb2736f8e6e59f
Darkann <60514789+Darkann@users.noreply.github.com>
2020-03-19 16:57:47 +0100

    new version

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ const app = express();
 app.listen(3000, () => {
     console.log("server started...");
 });
+
+app.get('/' , (req, res) => {
+    
+    console.log('access to / path');
+    res.send('hello world');
+});
+
