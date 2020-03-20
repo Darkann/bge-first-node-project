e2e26f3f47cb822978f5e8e8dc3355e4f1fdc588
Darkann <60514789+Darkann@users.noreply.github.com>
2020-03-20 09:32:00 +0100

    rajout html

--- index.js
+++ index.js
@@ -6,9 +6,20 @@ app.listen(3000, () => {
     console.log("server started...");
 });
 
-app.get('/' , (req, res) => {
-    
-    console.log('access to / path');
-    res.send('hello world');
+app.get("/", (req, res) => {
+    console.log("access to / path");
+    res.send(`<!DOCTYPE html>
+        < html >
+            <head>
+                <title>Project-2</title>
+            </head>
+            <body>
+                <h1>hello world!</h1>
+            </body>
+        </html >
+        `);
 });
 
+app.get("/about",(req, res) => {
+    res.send("about");
+});
