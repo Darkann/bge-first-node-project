950dd25cf1906654313c681b6f6e5bcb9cfb8016
Darkann <60514789+Darkann@users.noreply.github.com>
2020-03-20 11:07:30 +0100

    crea dossier public

--- index.js
+++ index.js
@@ -4,14 +4,5 @@ const app = express();
 app.listen(3000, () => {
     console.log("server started...");
 });
-app.get("/", (request, response) => {
-    console.log("access to / path");
-    const html = fs
-        .readFileSync("./src/index.html")
-        .toString ("utf8");
-        
-    response.send(html);
-});
-app.get("/about", (req, res) => {
-    res.send("About");
-});
+
+app.use(express.static("./public"));
